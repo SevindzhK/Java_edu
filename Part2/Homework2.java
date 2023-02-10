afe52ef2a483fb3efa6e4945bbc0686cd27813e8
SevindzhK <sevinge0709@gmail.com>
2023-02-10 12:27:35 +0300

    Changes after review

--- Part2/Homework2.java
+++ Part2/Homework2.java
@@ -7,8 +7,9 @@ import java.util.Scanner;
 
 public class Homework2 {
     public static void main(String[] args) {
-        //writerTxt();
-        System.out.println(isPalindrom());
+        writerTxt();
+        //System.out.println(isPalindrom());
+        //System.out.println(isPalindrom2());
     }
 
     private static boolean isPalindrom() {
@@ -17,24 +18,45 @@ public class Homework2 {
         do {
             System.out.println("Введите слово или предложение: ");
             str = scanner.nextLine();
-        } while (str.isEmpty());
+        } while (str.isBlank());
         scanner.close();
 
         str = str.toLowerCase().replace(" ", "");
-        StringBuilder sb = new StringBuilder(str);
-        sb = sb.reverse();
-        String reverse = sb.toString();
-        return str.equals(reverse);
+        StringBuilder reverse = new StringBuilder(str).reverse();
+
+        return str.equals(reverse.toString());
     }
+
+    private static boolean isPalindrom2() {
+        Scanner scanner = new Scanner(System.in);
+        String str = "";
+        do {
+            System.out.println("Введите слово или предложение: ");
+            str = scanner.nextLine();
+        } while (str.isBlank());
+        scanner.close();
+
+        str = str.toLowerCase().replace(" ", "");
+
+        for (int i = 0; i <= str.length()/2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 -i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 
     private static void writerTxt() {
         String str = "TEST";
         int count = 100;
-        StringBuilder text = new StringBuilder(str.repeat(count));
+        final String PATH = "src/main/resources/lib/hm.txt";
 
-        try (PrintWriter pw = new PrintWriter("src/main/resources/lib/hm.txt")) {
-            pw.print(text);
+        try (PrintWriter pw = new PrintWriter(PATH)) {
+            pw.print(str.repeat(count));
         } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
             throw new RuntimeException(e);
         }
     }
